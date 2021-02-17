8d865688f76412aad2b9c34147b13b014a586b36
Yecgaa1 <38446391+Yecgaa1@users.noreply.github.com>
2021-02-17 18:46:15 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/GitProject/Boat/ConnectionCabin/Core/Startup/startup_stm32f103c8tx.s" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Core/Startup/startup_stm32f103c8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/GitProject/Boat/ConnectionCabin/Core/Src/gpio.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Core/Src/gpio.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Core/Src/main.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Core/Src/main.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Core/Src/stm32f1xx_hal_msp.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Core/Src/stm32f1xx_it.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Core/Src/syscalls.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Core/Src/syscalls.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Core/Src/sysmem.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Core/Src/sysmem.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Core/Src/system_stm32f1xx.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "E:/GitProject/Boat/ConnectionCabin/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "E:/GitProject/Boat/ConnectionCabin/cmake-build-debug/CMakeFiles/ConnectionCabin.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
